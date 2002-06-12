febe2514343aa9ddc49ed45140a391a84ff449ab
Barry Warsaw <barry@python.org>
2002-06-12 15:54:01 +0000

    Code and comment cleanup.
    Also all `id' arguments are changed to `msgid' for consistency.

--- IMessageCatalog.py
+++ IMessageCatalog.py
@@ -13,45 +13,48 @@
 ##############################################################################
 """
 
-$Id: IMessageCatalog.py,v 1.2 2002/06/10 23:29:28 jim Exp $
+$Id: IMessageCatalog.py,v 1.3 2002/06/12 15:54:01 bwarsaw Exp $
 """
 
 from Interface import Interface
 
 
 class IMessageCatalog(Interface):
-    """Translation Service
+    """A catalog (mapping) of message ids to message text strings.
 
     This interface provides a method for translating a message or message id,
-    including text with interpolation. The message catalog basically serves as
-    a fairly simple mapping object.
+    including text with interpolation.  The message catalog basically serves
+    as a fairly simple mapping object.
 
-    A single Message Catalog represents a particular language and
-    domain. Therefore you will have the following constructor arguments:
+    A single message catalog represents a specific language and domain.
+    Therefore you will have the following constructor arguments:
 
-    language -- The language of the returning language. This is a read-only
+    language -- The language of the returned messages.  This is a read-only
                 attribute.
 
-    domain -- The translation domain for this messages. This is a read-only
-              attribute. See ITranslationService.
+    domain -- The translation domain for these messages.  This is a read-only
+              attribute.  See ITranslationService.
 
-    When we refer to text here, we mean text that follows the standard
-    Zope 3 text representation.
+    When we refer to text here, we mean text that follows the standard Zope 3
+    text representation.
     """
 
-    def getMessage(id):
-        """Return the appropriate text for the given id. As all get methods,
-           this will raise an error, if the id is not found
+    def getMessage(msgid):
+        """Get the appropriate text for the given message id.
+
+        An exception is raised if the message id is not found.
         """
 
-    def queryMessage(id, default=None):
-        """Return the appropriate test for the given id, but if the id is not
-           found, it should not raise an error, instead returning default. If
-           default is None, then the id itself is returned.
+    def queryMessage(msgid, default=None):
+        """Look for the appropriate text for the given message id.
+
+        If the message id is not found, no exception is raised.  Instead
+        default is returned, but if default is None, then msgid itself is
+        returned.
         """
-        
+
     def getLanguage():
-        """Return the langauge this message catalog is representing.
+        """Return the language this message catalog is representing.
         """
         
     def getDomain():
@@ -59,7 +62,8 @@ class IMessageCatalog(Interface):
         """
 
     def getIdentifier():
-        """Return a identifier for this message catalog. Note that this
-           identifier does not have to be unique as several message catalog
-           could serve the same domain and language.
+        """Return an identifier for this message catalog.
+
+        Note that this identifier does not have to be unique as several
+        message catalogs could be serving the same domain and language.
         """
