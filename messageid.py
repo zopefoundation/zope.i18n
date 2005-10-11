8e389de4d7b62ab2a6f5440a54272cd2e6e65347
Philipp von Weitershausen <philipp@weitershausen.de>
2005-10-11 18:36:20 +0000

    Deprecate this module as a former import location of MessageID (and apparently, Message).

--- messageid.py
+++ messageid.py
@@ -11,6 +11,20 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"$Id$"
+"""Provide a backwards compatible import location for i18n message
+ids.  This module will be removed from Zope 3.3.
+
+$Id$
+"""
+import warnings
+import zope.deprecation
+zope.deprecation.__show__.off()
 from zope.i18nmessageid import MessageID, MessageIDFactory
 from zope.i18nmessageid import Message, MessageFactory
+zope.deprecation.__show__.on()
+
+warnings.warn("The zope.i18n.messageid module as a backwards-compatible import "
+              "location for i18n message ids has been deprecated and will be "
+              "removed from Zope 3.3.  Please use Message and MessageFactory "
+              "from the zope.i18nmessageid package instead.",
+              DeprecationWarning, 2)
