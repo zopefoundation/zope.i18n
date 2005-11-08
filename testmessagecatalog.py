ef73a27a9145569e23ee92bdcfe00b76982ba938
Jim Fulton <jim@zope.com>
2005-11-08 20:00:30 +0000

    Added "test" message catalog for testing i18n.  If you specify ++lang++test in a URL, then all translated strings will be translated to [[domain][message_id], as in "[[zope][Preview]]". Text without the domain marker isn't translated.
    Unfortunately, currently you can't tell if it wasn't translated
    because it lacked a message id or if it wasn't translated because there
    was no message catalog.
    
    Yes, this is a new feature, but it's needed to write tests for bug fixes. :P

---
+++ testmessagecatalog.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Test message catalog
+
+$Id$
+"""
+
+from zope import interface
+from zope.i18n.interfaces import IGlobalMessageCatalog
+
+class TestMessageCatalog:
+    interface.implements(IGlobalMessageCatalog)
+
+    language = 'test'
+
+    def __init__(self, domain):
+        self.domain = domain
+
+    def queryMessage(self, msgid, default=None):
+        return u'[[%s][%s]]' % (self.domain, msgid)
+
+    getMessage = queryMessage
+
+    def getIdentifier(self):
+        return 'test'
+
+    def reload(self):
+        pass
+
+                     
