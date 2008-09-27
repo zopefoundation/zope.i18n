014f299604f6ae3c1169c89ac611c4c27aa9d6d0
Philipp von Weitershausen <philipp@weitershausen.de>
2008-09-27 10:20:00 +0000

    next version is 3.6.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name='zope.i18n',
-    version = '3.5.1dev',
+    version = '3.6.0dev',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description='Zope3 Internationalization Support',
