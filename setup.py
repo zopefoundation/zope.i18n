bf276f56de076b8c23f73fb6582ea2eb0a2bcbef
Hanno Schlichting <hanno@hannosch.eu>
2008-10-26 15:55:38 +0000

    Update after 3.6.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name='zope.i18n',
-    version = '3.6.0',
+    version = '3.6.1dev',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope3 Internationalization Support',
