f1ff8c8449ae6bee25b1b807f1c5e4e5a830074f
Chris McDonough <chrism@plope.com>
2009-12-14 13:55:43 +0000

    Back to dev.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
 
 setup(
     name='zope.i18n',
-    version = '3.7.2',
+    version = '3.7.3dev',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope3 Internationalization Support',
