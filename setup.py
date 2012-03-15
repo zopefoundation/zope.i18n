ccbdea77d5428599ffa03bfdf08b7bf23da150de
Gediminas Paulauskas <menesis@pov.lt>
2012-03-15 13:58:53 +0000

    Back to development: 3.8.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.i18n',
-    version='3.8.0',
+    version='3.8.1dev',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Internationalization Support',
