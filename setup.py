60a135be7bfe3e84cf1e76cd50cea20c879b43e5
Jason Madden <jamadden@gmail.com>
2017-12-18 11:50:40 -0600

    Back to development: 4.3.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ TESTS_REQUIRE = COMPILE_REQUIRES + ZCML_REQUIRES + [
 
 setup(
     name='zope.i18n',
-    version='4.3.0',
+    version='4.3.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Internationalization Support',
